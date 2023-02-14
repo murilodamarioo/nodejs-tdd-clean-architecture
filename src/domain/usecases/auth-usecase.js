1d2f87cefeaad6f622c3335877e8cf7fd9fac663
Murilo Damario <murilo.gcdamario@gmail.com>
2023-02-14 18:44:31 -0300

    simplify code

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -22,17 +22,13 @@ module.exports = class AuthUseCase {
     }
 
     const user = await this.loadUserByEmailRepository.load(email)
+    const isValid = user && await this.encrypter.compare(password, user.password)
 
-    if (!user) {
-      return null
-    }
-    const isValid = await this.encrypter.compare(password, user.password)
-
-    if (!isValid) {
-      return null
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id)
+      return accessToken
     }
 
-    const accessToken = await this.tokenGenerator.generate(user.id)
-    return accessToken
+    return null
   }
 }
