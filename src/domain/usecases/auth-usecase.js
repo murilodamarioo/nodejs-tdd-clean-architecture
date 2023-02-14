8d50d9867057c08f3dc108b3672ed8115fea6742
Murilo Damario <murilo.gcdamario@gmail.com>
2023-02-14 18:41:12 -0300

    ensure AuthUseCase returns an accessToken if valid credentials are provided

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -32,6 +32,7 @@ module.exports = class AuthUseCase {
       return null
     }
 
-    await this.tokenGenerator.generate(user.id)
+    const accessToken = await this.tokenGenerator.generate(user.id)
+    return accessToken
   }
 }
