910f429939f85d8308f91f9e64a1c625e352848e
Murilo Damario <murilo.gcdamario@gmail.com>
2023-02-17 22:04:02 -0300

    ensure LoadUserByEmailRepository throws if no userModel is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -44,5 +44,11 @@ describe('Load User By Email Repository', () => {
       _id: fakeUser.ops[0]._id,
       password: fakeUser.ops[0].password
     })
+
+    test('Should throw if no userModel is provided', async () => {
+      const { sut } = new LoadUserByEmailRepository()
+      const promise = sut.load('any_email@email.com')
+      expect(promise).rejects.toThrow()
+    })
   })
 })
